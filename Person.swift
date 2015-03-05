90d0c5662d8756c6e1c8141a365f4bd6aca87001
Gene Grinberg <Gene@blss-MacBook-Pro.local>
2015-03-05 13:14:29 -0500

    Database App Initial

---
+++ Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  TheList
+//
+//  Created by Gene Grinberg on 3/5/15.
+//  Copyright (c) 2015 Gene Grinberg. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSManagedObject {
+   
+}
